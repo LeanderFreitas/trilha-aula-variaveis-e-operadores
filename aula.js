8dbe8131868d8a19d1523cde9c41c1653424e182
Leander Freitas <leanderfreitas18@gmail.com>
2025-06-16 22:10:48 -0300

    desafio

--- aula.js
+++ aula.js
@@ -1,1 +1,1 @@
-let preco
+console.log ('ola')
---
+++ primeiro.desafio.js
@@ -0,0 +1,9 @@
+
+ const precoCombustivel = 5.79;
+ const kmPorLitros = 10;
+ const distanciaEmKm = 100;
+
+ const listrosConsumidos = distanciaEmKm / kmPorLitros;
+ const valorGastor = listrosConsumidos * precoCombustivel;
+ console.log(valorGastor);
+
