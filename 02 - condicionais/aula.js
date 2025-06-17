05d84e247960da14710e72f1bcf675ee4bd2b893
Leander Freitas <leanderfreitas18@gmail.com>
2025-06-17 13:22:36 -0300

    Update aula.js

--- 02 - condicionais/aula.js
+++ 02 - condicionais/aula.js
@@ -1,7 +1,10 @@
 const numero = 12;
 const numeroDivisivelPOr5 =  ( numero % 5) === 0;
-if ( numeroDivisivelPOr5){
+
+if (numero === 0){
     console.log('o numero é invalido');
+} else if(numeroDivisivelPOr5){
+    console.log('sim');
 }
 else{
     console.log('nao');
