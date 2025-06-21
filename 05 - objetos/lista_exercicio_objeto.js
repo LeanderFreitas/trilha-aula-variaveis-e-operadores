112ef608fdd76f21963bc1fa678114f3d5298250
Leander Freitas <leanderfreitas18@gmail.com>
2025-06-21 12:41:39 -0300

    primeiro exercicio de objeto

---
+++ 05 - objetos/lista_exercicio_objeto.js
@@ -0,0 +1,13 @@
+class marca{
+    marca;
+    cor;
+    gastoMedioPorKm;
+
+    constructor(marca,cor,gastoMedioPorKm){
+        this.marca;
+        this.cor;
+        this.gastoMedioPorKm = gastoMedioPorKm;
+    }
+}
+const uno = new Carro ('fiat','prata',1/12);
+console.log(uno);
