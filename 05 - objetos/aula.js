0d60a9410ff2a108f7f6a8da100379f047d408bb
Leander Freitas <leanderfreitas18@gmail.com>
2025-06-21 12:00:16 -0300

    Update aula.js

--- 05 - objetos/aula.js
+++ 05 - objetos/aula.js
@@ -12,5 +12,16 @@ class Pessoa{
             console.log('Meu nome é ${this.nome} e minha idade é ${this.idade}');
         }
     }
-    const vior = new Pessoa('vitor',25);
-    console.log(vitor);
+    function compararPessoas(p1,p2){
+        if(p1.idade > p2.idade){
+           console.log('${p1.nome} é mais velho(a) que ${p2.nome}');
+        }
+        else if(p1.idade > p2.idade){
+            console.log('${p2.nome} é mais velho(a) que ${p1.nome}');
+        }
+        else{
+             console.log('${p1.nome} é mais velho(a) que ${p2.nome}');
+        }
+    }
+    const vitor = new Pessoa('vitor',25);
+    const renan = new Pessoa('renan',30);
