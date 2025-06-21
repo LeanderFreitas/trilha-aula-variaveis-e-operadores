2907b60ac75fc09db4fef2d89aa5d7ff4a630833
Leander Freitas <leanderfreitas18@gmail.com>
2025-06-21 12:59:59 -0300

    Update lista_exercicio_objeto.js

--- 05 - objetos/lista_exercicio_objeto.js
+++ 05 - objetos/lista_exercicio_objeto.js
@@ -8,6 +8,11 @@ class marca{
         this.cor;
         this.gastoMedioPorKm = gastoMedioPorKm;
     }
+    calcularGastoDePercurso(distanciaEmKm,precoCombustivel){
+        return distanciaEmKm * this.gastoMedioPorKm *precoCombustivel;
+    }
 }
 const uno = new Carro ('fiat','prata',1/12);
-console.log(uno);
+console.log(uno.calcularGastoDePercurso(70,5));
+const palio = new Carro ('Fiat','preto' (1/10));
+console.log(palio.calcularGastoDePercurso(70,5));
