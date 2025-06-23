cb9d77f1ad9544ea51652bb3bb05a01534a6ab6d
Leander Freitas <leanderfreitas18@gmail.com>
2025-06-22 22:54:35 -0300

    Update exercicio2_array.js

--- 06 - lista (arrays)/exercicio2_array.js
+++ 06 - lista (arrays)/exercicio2_array.js
@@ -1,1 +1,8 @@
-const
+const numeros = [0,1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i < numeros.length; i++) {
+    const numeros = numeros[i];
+    if(numeros % 2 === 0){
+        console.log(numeros);
+    }
+}
